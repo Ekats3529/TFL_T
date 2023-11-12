a164f1938341104129c3bb948b612e9ad3981015
Ekats <suhowa.ekaterina2013@gmail.com>
2023-11-12 23:23:54 +0400

    done lab 1

--- lab1/main.py
+++ lab1/main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-states = Enum('states', ["S", 'Ai', 'Ac', 'As', 'Bs',
+states = Enum('states', ["S", 'Ai', 'Ac', 'As', 'Bs', "Cs",
                          'Ds', 'Gs', "E", "F"])
 
 lex = Enum('lex', ["do", 'loop', 'until',
@@ -14,13 +14,14 @@ class Lex:
 
     def __init__(self, type, value):
         self.type = type
-        self.val = value
+        self.value = value
 
 
 list_lex = []
 
 
 def add_lex(state, string, block):
+    # print("\t\t", state, string, block)
     if len(list_lex) > 0:
         prev_state = list_lex[-1].type
 
@@ -45,19 +46,28 @@ def add_lex(state, string, block):
             list_lex.append(Lex(lex.until, tmp))
             return True
         return False
+
     if tmp == "not":
         if prev_state in [lex.until, lex.lAnd, lex.lOr]:
             list_lex.append(Lex(lex.lNot, tmp))
             return True
         return False
+
     if tmp == "and":
         if prev_state == lex.var or prev_state == lex.const:
             list_lex.append(Lex(lex.lAnd, tmp))
             return True
         return False
+
     if tmp == "or":
         if prev_state == lex.var or prev_state == lex.const:
             list_lex.append(Lex(lex.lOr, tmp))
+            return True
+        return False
+
+    if tmp == "output":
+        if prev_state == lex.var or prev_state == lex.const:
+            list_lex.append(Lex(lex.output, tmp))
             return True
         return False
 
@@ -65,10 +75,16 @@ def add_lex(state, string, block):
         if prev_state == lex.var or prev_state == lex.const:
             list_lex.append(Lex(lex.rel, tmp))
             return True
+        if state == states.Ds:
+            prev_2_state = list_lex[-2].type if len(list_lex) > 2 else None
+            if prev_state == lex.var or prev_state == lex.const or prev_2_state == lex.var or prev_2_state == lex.const:
+                list_lex.append(Lex(lex.rel, tmp))
+                return True
         return False
 
     if state == states.Bs:
-        if prev_state == lex.var or prev_state == lex.const:
+        prev_2_state = list_lex[-2].type if len(list_lex) > 2 else None
+        if prev_state == lex.var or prev_state == lex.const or prev_2_state == lex.var or prev_2_state == lex.const:
             list_lex.append(Lex(lex.lAs, tmp))
             return True
         return False
@@ -78,13 +94,24 @@ def add_lex(state, string, block):
             list_lex.append(Lex(lex.ao, tmp))
             return True
         return False
+
     if state == states.Ai or state == states.Ac:
         if block == 0:
-            pass
+            if prev_state in [lex.lAs, lex.output, lex.do, lex.ao]:
+                if state == states.Ai:
+                    list_lex.append(Lex(lex.var, tmp))
+                if state == states.Ac:
+                    list_lex.append(Lex(lex.const, tmp))
+                return True
         elif block == 1:
-            pass
-        elif block == 2:
-            pass
+            if prev_state == lex.lAs or prev_state == lex.output:
+                return False
+            if state == states.Ai:
+                list_lex.append(Lex(lex.var, tmp))
+            if state == states.Ac:
+                list_lex.append(Lex(lex.const, tmp))
+            return True
+
         return False
 
 
@@ -93,15 +120,16 @@ def lex_analysis(text):
     prev_state = states.S
     add = False
     string = text
-    lex_start = ""
     i = 0
     type_expr = 0
 
     while i < len(string):
         lexm = ""
         while cur_state != states.E and cur_state != states.F and i < len(string):
+            # print(string[i], lexm, cur_state)
             prev_state = cur_state
-            add = True
+            add = False
+
             if cur_state == states.S:
                 if string[i] == " ":
                     cur_state = states.F
@@ -118,8 +146,6 @@ def lex_analysis(text):
                     cur_state = states.Bs
                 elif string[i] in ['*', '-', '+', '-']:
                     cur_state = states.Cs
-                elif string[i] == 0:
-                    cur_state = states.F
                 else:
                     cur_state = states.E
                     add = False
@@ -127,6 +153,7 @@ def lex_analysis(text):
             elif cur_state == states.Ai:
                 if string[i] == " ":
                     cur_state = states.S
+                    add = True
                 elif string[i].isalnum():
                     add = False
                 elif string[i] == "<":
@@ -137,8 +164,6 @@ def lex_analysis(text):
                     cur_state = states.Bs
                 elif string[i] in ['*', '-', '+', '-']:
                     cur_state = states.Cs
-                elif string[i] == 0:
-                    cur_state = states.F
                 else:
                     cur_state = states.E
                     add = False
@@ -146,6 +171,7 @@ def lex_analysis(text):
             elif cur_state == states.Ac:
                 if string[i] == " ":
                     cur_state = states.S
+                    add = True
                 elif string[i].isdigit():
                     add = False
                 elif string[i] == "<":
@@ -156,8 +182,6 @@ def lex_analysis(text):
                     cur_state = states.Bs
                 elif string[i] in ['*', '-', '+', '-']:
                     cur_state = states.Cs
-                elif string[i] == 0:
-                    cur_state = states.F
                 else:
                     cur_state = states.E
                     add = False
@@ -165,14 +189,13 @@ def lex_analysis(text):
             elif cur_state == states.As:
                 if string[i] == " ":
                     cur_state = states.S
+                    add = True
                 elif string[i].isalpha():
                     cur_state = states.Ai
                 elif string[i].isdigit():
                     cur_state = states.Ac
                 elif string[i] == ">":
                     cur_state = states.Ds
-                elif string[i] == 0:
-                    cur_state = states.F
                 else:
                     cur_state = states.E
                     add = False
@@ -180,14 +203,13 @@ def lex_analysis(text):
             elif cur_state == states.Bs:
                 if string[i] == " ":
                     cur_state = states.S
+                    add = True
                 elif string[i].isalpha():
                     cur_state = states.Ai
                 elif string[i].isdigit():
                     cur_state = states.Ac
                 elif string[i] == "=":
                     cur_state = states.Gs
-                elif string[i] == 0:
-                    cur_state = states.F
                 else:
                     cur_state = states.E
                     add = False
@@ -195,12 +217,23 @@ def lex_analysis(text):
             elif cur_state == states.Ds:
                 if string[i] == " ":
                     cur_state = states.S
+                    add = True
                 elif string[i].isalpha():
                     cur_state = states.Ai
                 elif string[i].isdigit():
                     cur_state = states.Ac
-                elif string[i] == 0:
-                    cur_state = states.F
+                else:
+                    cur_state = states.E
+                    add = False
+
+            elif cur_state == states.Cs:
+                if string[i] == " ":
+                    cur_state = states.S
+                    add = True
+                elif string[i].isalpha():
+                    cur_state = states.Ai
+                elif string[i].isdigit():
+                    cur_state = states.Ac
                 else:
                     cur_state = states.E
                     add = False
@@ -209,26 +242,33 @@ def lex_analysis(text):
                 return False
 
             if add:
-                lexm = "".join(lexm.split())
+                lexm = "".join(lexm.strip().split())
                 if len(lexm) == 0:
                     i += 1
                     continue
                 if len(lexm) > 0:
-                    pass
-                if len(list_lex) > 0 and list_lex[-1].type == lex.do:
-                    type_expr = 1
-                add_lex(prev_state, lexm, type_expr)
+                    if not add_lex(prev_state, lexm, type_expr):
+                        return False
+
+                lexm = ""
+
+            if len(list_lex) > 0 and list_lex[-1].type == lex.until:
+                type_expr = 1
 
             if cur_state != states.E and cur_state != states.F:
                 lexm += string[i]
                 i += 1
+    if type_expr == 0 or list_lex[-1].type not in [lex.var, lex.const]:
+        return False
+
+    return True
 
 
 if __name__ == "__main__":
     text = input("Enter the text: \n")
     text = text.strip()
-    if lex_analysis(text):
-        print("Text is a part of the program")
+    if lex_analysis(text + " "):
+        print("Text is a part of the program\n")
         for x in list_lex:
             print(x.value, x.type)
     else:
